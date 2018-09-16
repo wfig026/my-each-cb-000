860597a8692f8149d42062736c7c910cb4fb3794
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 19:08:08 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,8 @@
 def my_each(array)
   i = 0
-
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    array
-
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
 end
